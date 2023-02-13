c7ad1a7dcc13e5f53c3be9d572f9f18223b2ec13
dscdorothy <123066553+dscdorothy@users.noreply.github.com>
2023-02-13 15:48:56 +0800

    Update and rename Fig.1_R_for_PCoA.R to Fig.1_R_for_diversity.R

--- Fig.1_R_for_diversity.R
+++ Fig.1_R_for_diversity.R
@@ -23,3 +23,33 @@ anosim(otu.bry,G$dm_diagnosed,permutations = 999)
 
 #rarecurve
 rarecurve(otu)
+
+#Alpha-diversity
+library(picante)     
+
+alpha <- function(x, tree = NULL, base = exp(1)) {
+  est <- estimateR(x)
+  Richness <- est[1, ]
+  Chao1 <- est[2, ]
+  ACE <- est[4, ]
+  Shannon <- diversity(x, index = 'shannon', base = base)
+  Simpson <- diversity(x, index = 'simpson')    #Gini-Simpson 指数
+  Inverse.Simpson <- diversity(otu, index = "inv")
+  Pielou <- Shannon / log(Richness, base)
+  goods_coverage <- 1 - rowSums(x == 1) / rowSums(x)
+  
+  result <- data.frame(Richness, Shannon, Simpson, Inverse.Simpson,Pielou, Chao1, ACE, goods_coverage)
+  if (!is.null(tree)) {
+    PD_whole_tree <- pd(x, tree, include.root = FALSE)[1]
+    names(PD_whole_tree) <- 'PD_whole_tree'
+    result <- cbind(result, PD_whole_tree)
+  }
+  result
+}
+
+otu <- read.delim('otu_table.txt', row.names = 1, sep = '\t', stringsAsFactors = FALSE, check.names = FALSE)
+otu <- t(otu)
+tree <- read.tree('otu_tree.tre')
+
+alpha_all <- alpha(otu, base = 2)
+write.csv(alpha_all, 'otu_alpha.csv', quote = FALSE)
